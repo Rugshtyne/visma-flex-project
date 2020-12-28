6c4e47d91ceafb9f9ef0783d9d1ce2fd23f91cb4
Rugshtyne <leonixaz@gmail.com>
2020-12-28 18:32:25 +0200

    Level component created

--- src/containers/FlexinFlex/FlexinFlex.js
+++ src/containers/FlexinFlex/FlexinFlex.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Menu from "../../components/Menu/Menu";
+import Level from '../Level/Level';
 
 function FlexinFlex(props) {
     return (
         <React.Fragment>
             <h1>I'm FlexinFlex</h1>
-            <Menu></Menu>
+            {/* <Menu></Menu> */}
+            <Level></Level>
         </React.Fragment>
     );
 }
---
+++ src/containers/Level/Level.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Level(props) {
+    return (
+        <h2>This is a level</h2>
+    );
+}
+
+export default Level;
