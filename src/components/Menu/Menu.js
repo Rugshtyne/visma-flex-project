fd8c38c5399efaead7f2b6d87f49345baba1c4a9
Rugshtyne <leonixaz@gmail.com>
2020-12-28 18:27:40 +0200

    Setting up components/containers

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Menu(props) {
+    return (
+        <h2>This will be menu with menu items</h2>
+    );
+}
+
+export default Menu;
---
+++ src/containers/FlexinFlex/FlexinFlex.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Menu from "../../components/Menu/Menu";
+
+function FlexinFlex(props) {
+    return (
+        <React.Fragment>
+            <h1>I'm FlexinFlex</h1>
+            <Menu></Menu>
+        </React.Fragment>
+    );
+}
+
+export default FlexinFlex;
